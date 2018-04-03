ac1733829419f318a9ac2320d6a54db063a60906
rjgamer <jonathan.nessier@neoflow.ch>
2018-04-03 11:19:07 +0200

    Added dummy module

---
+++ modules/dummy/classes/Neoflow/Module/Dummy/Manager.php
@@ -0,0 +1,56 @@
+<?php
+namespace Neoflow\Module\Dummy;
+
+use Neoflow\CMS\Manager\AbstractModuleManager;
+
+class Manager extends AbstractModuleManager
+{
+
+    /**
+     * Install Dummy module.
+     *
+     * @return bool
+     */
+    public function install(): bool
+    {
+        // Create tables
+
+        return true;
+    }
+
+    /**
+     * Uninstall Dummy module.
+     *
+     * @return bool
+     */
+    public function uninstall(): bool
+    {
+        // Drop tables
+
+        return true;
+    }
+
+    /**
+     * Initialize Dummy module.
+     *
+     * @return bool
+     */
+    public function initialize(): bool
+    {
+        // Initialize module (e.g. register service to App instance)
+
+        return true;
+    }
+
+    /**
+     * Update Dummy module.
+     *
+     * @return bool
+     */
+    public function update(): bool
+    {
+        // Alter tables
+
+        return true;
+    }
+}
---
+++ modules/dummy/views/dummy/index.php
@@ -0,0 +1,24 @@
+<div class="row">
+    <div class="col-xl-7">
+
+        <div class="card">
+            <h4 class="card-header">
+                <?= translate('Dummy page'); ?>
+            </h4>
+
+            <div class="card-body">
+
+                <p>
+                    <?= translate('Dummy page description') ?>
+                </p>
+
+            </div>
+
+        </div>
+
+    </div>
+    <div class="col-xl-5">
+
+    </div>
+</div>
+
