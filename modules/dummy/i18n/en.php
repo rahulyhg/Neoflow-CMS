2b72fcb62e060366318b7382d5183efe858b56d4
rjgamer <jonathan.nessier@neoflow.ch>
2018-04-03 16:24:03 +0200

    Nothing to see :)

--- modules/dummy/i18n/en.php
+++
@@ -1,7 +0,0 @@
-<?php
-$this->addTranslation([
-    'Dummy' => 'Dummy',
-    'Dummy page' => 'Dummy page',
-    'Dummy page description' => 'Dummy page of the dummy module as a demo example for interested developers.',
-    'Dummy module as demo example for interested developers.' => 'Dummy module as demo example for interested developers.'
-]);
--- modules/dummy/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Module routes
-return [
-    'routes' => [
-        ['tmod_dummy_backend_index', 'get', '/backend/module/dummy', 'Backend@index'],
-    ],
-    'namespace' => '\\Neoflow\\Module\\Dummy\\Controller\\',
-];
