f2c03ab028b1b6360e4e85d7d4f54ee44a810906
rjgamer <jonathan.nessier@neoflow.ch>
2018-04-19 14:29:54 +0200

    Fixed section content template

--- themes/flatly/templates/frontend/section-content.php
+++ themes/flatly/templates/frontend/section-content.php
@@ -1,6 +1,5 @@
 <div class="section-content block-<?= $block->block_key; ?>" id="section-<?= $section->id(); ?>">
     <div class="container">
         <?= $content; ?>
-        <p><?= $block->block_key; ?></p>
     </div>
 </div>
