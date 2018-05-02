66b2d6a025fefca2ca5bad443672ae7f66730402
rjgamer <jonathan.nessier@neoflow.ch>
2018-05-02 16:34:23 +0200

    Updated TinyMCE to 4.7.11

--- modules/tinymce/info.php
+++ modules/tinymce/info.php
@@ -5,11 +5,14 @@ return [
     'identifier' => 'tinymce',
     'dependencies' => 'wysiwyg',
     'description' => 'HTML text editor, designed to simplify website content creation. Extends the WYSIWYG editor module.',
-    'version' => '4.7.1',
+    'version' => '4.7.11',
+    'for' => [
+        '4.7.1'
+    ],
     'author' => 'Jonathan Nessier <jonathan.nessier@neoflow.ch>',
     'copyright' => 'Copyright (c) Jonathan Nessier, Neoflow.ch',
     'license' => 'MIT',
     'type' => 'library',
     'folder_name' => 'tinymce',
-    'manager_class' => '\Neoflow\Module\TinyMCE\Manager',
+    'manager_class' => '\Neoflow\Module\TinyMCE\Manager'
 ];
