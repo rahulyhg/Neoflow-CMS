ce7272475dc1c01c5f2ea96ae0121f116c8485d3
rjgamer <jonathan.nessier@neoflow.ch>
2018-05-11 14:37:37 +0200

    Added framework folder delete

--- update/classes/Neoflow/CMS/UpdateManager.php
+++ update/classes/Neoflow/CMS/UpdateManager.php
@@ -1,9 +1,23 @@
 <?php
-
 namespace Neoflow\CMS;
 
 use Neoflow\CMS\Manager\AbstractUpdateManager;
+use Neoflow\Filesystem\Folder;
 
 class UpdateManager extends AbstractUpdateManager
 {
+
+    /**
+     * Install update.
+     *
+     * @return bool
+     */
+    public function install(): bool
+    {
+        if (parent::install()) {
+            $frameworkFolder = new Folder($this->config()->getPath('/framework'));
+            return $frameworkFolder->delete();
+        }
+        return false;
+    }
 }
