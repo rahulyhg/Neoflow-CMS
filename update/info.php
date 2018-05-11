17efa276c9b7c9ca54d8f1f89f6fc9eb7acd143a
rjgamer <jonathan.nessier@neoflow.ch>
2018-05-11 14:18:31 +0200

    Updated update info

--- update/info.php
+++ update/info.php
@@ -1,5 +1,4 @@
 <?php
-
 return [
     'version' => '1.0.0-a2',
     'for' => [
@@ -8,9 +7,16 @@ return [
     'sql' => 'changes.sql',
     'files' => 'files',
     'modules' => [
+        'code' => 'code.zip',
+        'datetimepicker' => 'datetimepicker.zip',
         'dummy' => 'dummy.zip',
+        'search' => 'search.zip',
+        'sitemap' => 'sitemap.zip',
+        'snippets' => 'snippets.zip',
+        'wysiwyg' => 'wysiwyg.zip',
     ],
     'themes' => [
         'flatly' => 'flatly.zip',
+        'neoflow-backend' => 'neoflow-backend.zip',
     ],
 ];
