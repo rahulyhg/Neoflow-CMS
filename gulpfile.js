e265a0b4ce3b1d03a96fcade0b82093c6833b941
rjgamer <jonathan.nessier@neoflow.ch>
2018-05-11 14:05:21 +0200

    Optimized update building

--- gulpfile.js
+++ gulpfile.js
@@ -59,32 +59,6 @@ gulp.task('update:release', function (callback) {
     return runSequence('update:_getTagForGit', 'update:_checkoutFromGit', 'update:_copyVendor', 'update:_createModuleZipPackages', 'update:_createThemeZipPackages', 'update:_createZipPackage', callback);
 });
 
-// Create zip file for update
-gulp.task('update:_createZipPackage', function () {
-    console.log('Create ' + pjson.name + '-' + pjson.version + '-update.zip');
-    return gulp
-            .src([
-                './update/**',
-                '!./update/install/config.php',
-                '!./update/install/package*',
-                '!./update/install/composer*',
-                '!./update/install/gulpfile.js',
-                '!./update/install/node_modules{,/**}',
-                '!./update/install/installation{,/**}',
-                '!./update/install/nbproject{,/**}',
-                '!./update/install/src{,/**}',
-                '!./update/install/modules{,/**}',
-                '!./update/install/robots.txt',
-                '!./update/install/sitemap.xml',
-                '!./update/install/temp{,/**}',
-                '!./update/install/media/modules/wysiwyg{,/**}',
-                '!./update/install/themes{,/**}',
-                '!./update/install/installation{,/**}',
-                '!./update/*.zip'
-            ])
-            .pipe(zip(pjson.name + '-update-' + pjson.version + '.zip'))
-            .pipe(gulp.dest('./update/'));
-});
 
 // Last commit or tag
 var tag = '';
@@ -92,7 +66,7 @@ var tag = '';
 // Show prompt to get last commit or tag for checkout
 gulp.task('update:_getTagForGit', function () {
     return gulp
-            .src('./update/install')
+            .src('./update/files')
             .pipe(prompt.prompt({
                 type: 'input',
                 name: 'tag',
@@ -105,18 +79,46 @@ gulp.task('update:_getTagForGit', function () {
 // Checkout files and folders from Git based on last commit or tag
 gulp.task('update:_checkoutFromGit', function () {
     return gulp
-            .src('./update/install')
+            .src('./update/files')
             .pipe(run('git checkout-index -f --prefix="<%= file.path %>/" $(git diff --name-only ' + tag + ')', {
                 usePowerShell: true,
                 verbosity: 0
             }));
 });
 
+// Create zip file for update
+gulp.task('update:_createZipPackage', function () {
+    console.log('Create ' + pjson.name + '-' + tag + '-to-' + pjson.version + '-update.zip');
+    return gulp
+            .src([
+                './update/**',
+                '!./update/files/config.php',
+                '!./update/files/package*',
+                '!./update/files/composer*',
+                '!./update/files/gulpfile.js',
+                '!./update/files/node_modules{,/**}',
+                '!./update/files/installation{,/**}',
+                '!./update/files/nbproject{,/**}',
+                '!./update/files/src{,/**}',
+                '!./update/files/modules{,/**}',
+                '!./update/files/robots.txt',
+                '!./update/files/sitemap.xml',
+                '!./update/files/temp{,/**}',
+                '!./update/files/media/modules/wysiwyg{,/**}',
+                '!./update/files/themes{,/**}',
+                '!./update/files/installation{,/**}',
+                '!./update/*.zip'
+            ])
+            .pipe(zip(pjson.name + '-update-' + tag + '-to-' + pjson.version + '.zip'))
+            .pipe(gulp.dest('./update/'));
+});
+
+
 // Copy all vendor files and folders to the update
 gulp.task('update:_copyVendor', function () {
     return gulp
             .src('./vendor/**/*')
-            .pipe(gulp.dest('./update/install/vendor'));
+            .pipe(gulp.dest('./update/files/vendor'));
 });
 
 // Create zip file of each core module (incl. Dummy module)
@@ -128,10 +130,10 @@ gulp.task('update:_createModuleZipPackages', function () {
     ];
 
     // Add folder names of changed modules
-    fs.lstat('./update/install/modules', (err, stats) => {
+    fs.lstat('./update/files/modules', (err, stats) => {
         if (stats && stats.isDirectory()) {
             coreModuleFolders.concat(fs
-                    .readdirSync('./update/install/modules')
+                    .readdirSync('./update/files/modules')
                     .filter(function (file) {
                         return fs.statSync(path.join('./modules', file)).isDirectory();
                     }));
@@ -168,10 +170,10 @@ gulp.task('update:_createThemeZipPackages', function () {
     ];
 
     // Add folder names of changed modules
-    fs.lstat('./update/install/themes', (err, stats) => {
+    fs.lstat('./update/files/themes', (err, stats) => {
         if (stats && stats.isDirectory()) {
             coreThemeFolders.concat(fs
-                    .readdirSync('./update/install/themes')
+                    .readdirSync('./update/files/themes')
                     .filter(function (file) {
                         return fs.statSync(path.join('./themes', file)).isDirectory();
                     }));
