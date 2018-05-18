12843dda487c2e85cb25f7ed3d68e5dcaf9bc140
rjgamer <jonathan.nessier@neoflow.ch>
2018-05-18 22:14:25 +0200

    Fixed sql

--- update/delivery/changes.sql
+++ update/delivery/changes.sql
@@ -2,6 +2,6 @@ ALTER TABLE `settings` CHANGE `author` `website_author` VARCHAR(50) DEFAULT NULL
 ALTER TABLE `settings` CHANGE `keywords` `website_keywords` VARCHAR(250) DEFAULT NULL;
 ALTER TABLE `settings` CHANGE `sender_emailaddress` `emailaddress` VARCHAR(100) DEFAULT NULL;
 
-DROP `visitors`;
+DROP TABLE `visitors`;
 
 UPDATE `permissions` SET `permission_key` = 'manage_themes' WHERE `permissions`.`permission_id` = 4;
