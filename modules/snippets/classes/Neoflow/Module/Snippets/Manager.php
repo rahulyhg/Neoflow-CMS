b22a3c3627cd79c2b532a2f364157b1f6d855bec
rjgamer <jonathan.nessier@outlook.com>
2018-06-07 18:29:47 +0200

    Fixed wrong sql

--- modules/snippets/classes/Neoflow/Module/Snippets/Manager.php
+++ modules/snippets/classes/Neoflow/Module/Snippets/Manager.php
@@ -14,21 +14,19 @@ class Manager extends AbstractModuleManager
     public function install(): bool
     {
         if (!$this->database()->hasTable('mod_snippets')) {
-            $this
-                ->database()
-                ->prepare('
+            $this->database()->prepare('
                         CREATE TABLE `mod_snippets` (
-                            `code_id` INT NOT NULL AUTO_INCREMENT,
-                            `title` VARCHAR(100) NOT NULL,
-                            `code` TEXT NOT NULL,
-                            `placeholder` VARCHAR(100) NOT NULL,
-                            `description` TEXT NOT NULL,
-                        PRIMARY KEY (`code_id`));
+                            `snippet_id` int(11) NOT NULL AUTO_INCREMENT,
+                            `title` varchar(100) NOT NULL,
+                            `code` text NOT NULL,
+                            `placeholder` varchar(100) NOT NULL,
+                            `description` text DEFAULT NULL,
+                            `parameters` varchar(100) DEFAULT NULL,
+                        PRIMARY KEY (`snippet_id`));
 
                         INSERT INTO `mod_snippets` VALUES(1, "Dummy", "return \"Just a snippet\";", "dummy", "Dummy snippet....", "");
-                        INSERT INTO `mod_snippets` VALUES(2, "Google Analytics", "return \"<script>\r\n	(function (i, s, o, g, r, a, m) {\r\n		i[\"GoogleAnalyticsObject\"] = r;\r\n		i[r] = i[r] || function () {\r\n			(i[r].q = i[r].q || []).push(arguments)\r\n		}, i[r].l = 1 * new Date();\r\n		a = s.createElement(o),\r\n				m = s.getElementsByTagName(o)[0];\r\n		a.async = 1;\r\n		a.src = g;\r\n		m.parentNode.insertBefore(a, m);\r\n	})(window, document, \"script\", \"https://www.google-analytics.com/analytics.js\", \"ga\");\r\n\r\n	ga(\"create\", \"\" . $id . \"\", \"auto\");\r\n	ga(\"send\", \"pageview\");\r\n</script>\";", "GoogleAnalytics", "Creates the JavaScript code for Google Analytics based on an custom ID which passed by a parameter.", "id");
-                        ')
-                ->execute();
+                        INSERT INTO `mod_snippets` VALUES(2, "Google Analytics", "return \'<script>\r\n	(function (i, s, o, g, r, a, m) {\r\n		i[\"GoogleAnalyticsObject\"] = r;\r\n		i[r] = i[r] || function () {\r\n			(i[r].q = i[r].q || []).push(arguments)\r\n		}, i[r].l = 1 * new Date();\r\n		a = s.createElement(o),\r\n				m = s.getElementsByTagName(o)[0];\r\n		a.async = 1;\r\n		a.src = g;\r\n		m.parentNode.insertBefore(a, m);\r\n	})(window, document, \"script\", \"https://www.google-analytics.com/analytics.js\", \"ga\");\r\n\r\n	ga(\"create\", \"\' . $id . \'\", \"auto\");\r\n	ga(\"send\", \"pageview\");\r\n</script>\';", "GoogleAnalytics", "Creates the JavaScript code for Google Analytics based on an custom ID which passed by a parameter.", "id");
+                        ')->execute();
         }
 
         return false;
@@ -42,10 +40,7 @@ class Manager extends AbstractModuleManager
     public function uninstall(): bool
     {
         if ($this->database()->hasTable('mod_snippets')) {
-            return $this
-                    ->database()
-                    ->prepare('DROP TABLE `mod_snippets`')
-                    ->execute();
+            return $this->database()->prepare('DROP TABLE `mod_snippets`')->execute();
         }
 
         return true;
