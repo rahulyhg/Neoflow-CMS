2c803de4982a210ed7fd2d6e14f13c4fe15a693e
rjgamer <jonathan.nessier@neoflow.ch>
2018-06-18 16:32:08 +0200

    Fixed PHP code styling

--- modules/wysiwyg/templates/wysiwyg/editor.php
+++ modules/wysiwyg/templates/wysiwyg/editor.php
@@ -1,1 +1,7 @@
-<textarea name="<?= $name ?>" <?= ($id ? 'id="'.$id.'"' : '') ?> <?= ($height ? 'height="'.$height.'"' : '') ?> class="form-control" rows="10" width="100%"><?= $content ?></textarea>
+<?php
+
+$id = ($id ? 'id="' . $id . '"' : '');
+$height = ($height ? 'height="' . $height . '"' : '');
+
+?>
+<textarea name="<?= $name ?>" <?= $id ?> <?= $height ?> class="form-control" rows="10" width="100%"><?= $content ?></textarea>
